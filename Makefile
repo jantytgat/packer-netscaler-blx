606740c30010a56fbc2728441783e8d9a41d0825
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-05-15 10:44:08 +0200

    Update launch script and Makefile to enable logging and build file specification
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ default:
 
 ubuntu-blx-22-04:
 	packer init "./linux/ubuntu-blx-22.04"
-	sh ./scripts/ubuntu-blx-22.04.sh
+	sh ./scripts/ubuntu-blx-22.04.sh build.pkrvars.hcl
 	sh ./scripts/cleanup.sh ./linux/ubuntu-blx-22.04
 
 clean:
--- scripts/ubuntu-blx-22.04.sh
+++ scripts/ubuntu-blx-22.04.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+INPUT=$1
 eval $(op signin)
 cd linux/ubuntu-blx-22.04
 
 git log -n 1 > build.auto.proxmox.data
 
-op inject -i build.pkrvars.hcl -o build.auto.pkrvars.hcl
-packer build -timestamp-ui .
+op inject -i $INPUT -o build.auto.pkrvars.hcl
+PACKER_LOG="yes" PACKER_LOG_PATH="debug.log" packer build -timestamp-ui .
